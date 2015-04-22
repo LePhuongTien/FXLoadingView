393bb2c1e9e1573026120db37096f6ded2117859
Le Phuong Tien <lephuongtien.fx@gmail.com>
2015-04-22 16:33:09 +0700

    finished

---
+++ FXLoadingView/FXLoadingView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FXLoadingView
+//
+//  Created by Le Phuong Tien on 4/22/15.
+//  Copyright (c) 2015 Le Phuong Tien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
